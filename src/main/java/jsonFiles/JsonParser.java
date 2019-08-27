85d5ce1a4d9cf26e109b7fa65b426fcac8a3b5f7
Olga Karpenko <okarpenko@usfca.edu>
2019-08-27 00:40:43 -0700

    Added a Singleton example. Added data stuctures example.

--- src/main/java/jsonFiles/JsonParser.java
+++ src/main/java/jsonFiles/JsonParser.java
@@ -5,8 +5,8 @@ import com.google.gson.stream.JsonReader;
 
 import java.io.FileReader;
 import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 /** The example demonstrates reading from a JSON file */
 public class JsonParser {
@@ -14,8 +14,14 @@ public class JsonParser {
         JsonParser jp = new JsonParser();
         jp.parseSimple("src/main/java/jsonFiles/exJsonSimple.json");
         //jp.parse("src/main/java/jsonFiles/exJsonSimple.json");
+        //jp.parseJSONObjectWithArray("src/main/java/jsonFiles/exJsonWithArray.json");
     }
 
+    /** This method demonstrates how to parse a simple json file
+     * that contains info about fruits; using GSON library.
+     *
+     * @param filePath path to the json file
+     */
     public void parseSimple(String filePath) {
         Gson gson = new Gson();
 
@@ -23,13 +29,16 @@ public class JsonParser {
             Fruit fruit = gson.fromJson(br, Fruit.class);
             System.out.println(fruit);
         }
-
         catch(IOException e) {
             System.out.println("Could not read the file");
         }
 
     }
 
+    /** Demonstrates another way of reading json files using GSON library
+     *
+     * @param filePath path to the file
+     */
     public void parse(String filePath) {
         try (JsonReader jsonReader = new JsonReader(new FileReader(filePath))) {
             jsonReader.beginObject();
@@ -56,6 +65,63 @@ public class JsonParser {
 
         } catch (IOException e) {
             System.out.println("Could not read from file");
+        }
+    }
+
+    /** Shows a way to read a JSON that contains a JSON array
+     * a
+     * @param filePath
+     */
+    public void parseJSONObjectWithArray(String filePath) {
+        try (JsonReader jsonReader = new JsonReader(new FileReader(filePath))) {
+            jsonReader.beginObject();
+
+            List<Fruit> fruits = new ArrayList<Fruit>();
+
+            if (jsonReader.hasNext()) {
+
+                String name = jsonReader.nextName();
+                if (name.equals("fruits")) {
+                    // Read Json Array
+                    jsonReader.beginArray();
+                    String nameOfFruit = "";
+                    double price = 0;
+                    String color = "";
+                    boolean isOrganic = true;
+
+                    while (jsonReader.hasNext()) {
+                        jsonReader.beginObject();
+                        while (jsonReader.hasNext()) {
+                            String name1 = jsonReader.nextName();
+                            if (name1.equals("name")) {
+                                nameOfFruit = jsonReader.nextString();
+                                System.out.print(nameOfFruit + ", ");
+                            } else if (name1.equals("price")) {
+                                price = jsonReader.nextDouble();
+                                System.out.print(price + ", ");
+                            } else if (name1.equals("color")) {
+                                color = jsonReader.nextString();
+                                System.out.print(color + ", ");
+                            } else if (name1.equals("organic")) {
+                                isOrganic = jsonReader.nextBoolean();
+                                System.out.println(isOrganic);
+                            }
+                        }
+
+                        Fruit f = new Fruit(nameOfFruit, color, price, isOrganic);
+                        fruits.add(f);
+                        jsonReader.endObject();
+
+                    }
+                    jsonReader.endArray();
+
+                }
+
+            }
+            jsonReader.endObject();
+        }
+        catch (IOException e) {
+            System.out.println("Could not read from file: " + e);
         }
     }
 }
