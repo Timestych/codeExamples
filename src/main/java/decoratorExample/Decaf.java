2ac2ac605807e4c3a8863eb8a34920c2e3290cf2
Olga Karpenko <okarpenko@usfca.edu>
2019-09-04 10:23:24 -0700

    Decorator, factory and abstract classes examples.

---
+++ src/main/java/decoratorExample/Decaf.java
@@ -0,0 +1,16 @@
+package decoratorExample;
+
+/** The example demonstrates how to use the Decorator pattern.
+ *  The code is from Head First Design Pattern book.
+ *
+ */
+public class Decaf extends Beverage {
+	public Decaf() {
+		description = "Decaf Coffee";
+	}
+ 
+	public double cost() {
+		return 1.05;
+	}
+}
+
---
+++ src/main/java/decoratorExample/Espresso.java
@@ -0,0 +1,17 @@
+package decoratorExample;
+
+/** The example demonstrates how to use the Decorator pattern.
+ *  The code is from Head First Design Pattern book.
+ *
+ */
+public class Espresso extends Beverage {
+  
+	public Espresso() {
+		description = "Espresso";
+	}
+  
+	public double cost() {
+		return 1.99;
+	}
+}
+
